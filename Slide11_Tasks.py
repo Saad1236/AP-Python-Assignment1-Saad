ca0bab09c7bd444ee91d7a60424aaa161e853760
Saad1236 <saadalibabar1996@gmail.com>
2022-10-04 13:54:49 +0500

    1-A few minor changes

--- Slide11_Tasks.py
+++ Slide11_Tasks.py
@@ -11,7 +11,7 @@ print("Variable B after swap: ", b)
 ##### Factorial #####
 print("Task 2=Factorial Calculator")
 fact = 1
-numb = int(input("Enter Number: "))
+numb = int(input("Enter Number to find Factorial: "))
 for i in range(numb):
     fact = numb * fact
     numb = numb-1
@@ -22,7 +22,7 @@ print("Factorial is:", fact)
 
 #### Fibonacci Series ####
 print("Task 3=Fibonacci Series")
-n = int(input("Enter index up to which series is to be printed): "))
+n = int(input("Enter index (up to which series is to be printed): "))
 k = 0
 j = 1
 if n <= 0:
@@ -41,11 +41,45 @@ print("Task 4=Check String Operations")
 string1 = input("Enter string to find its length: ")
 print("Length of string is: ", len(string1))
 
+print("Enter Strings and sub-string to be checked:")
 string1 = input("Enter string1: ")
-string2 = input("Enter sub-string to be checked: ")
-if string2 == '':
-    print("Empty Substring present by default")
-elif string2 in string1:
-    print("Substring found")
-else:
-    print("Substring not found")
+string2 = input("Enter sub-string: ")
+n = 0
+for char in string1:
+    for cha in string2:
+        if char == char:
+            n = n + 1
+    if n == len(string2):
+        print("Substring Found at", n)
+        break
+    else:
+        print("Substring not found")
+
+print("Enter Strings to be concatenated")
+string1 = input("String1: ")
+string2 = input("String2: ")
+print(string1 + string2)
+
+##### Calculator #####
+print("Task 5=Calculator")
+
+
+def calculator(Op1, Op2):
+    select = input("Choose the Operator + - * / ")
+    if select == "+":
+        print("Sum of the two digits is: ", Op1 + Op2)
+    elif select == "-":
+        print("2nd minus 1st is: ", Op1 - Op2)
+    elif select == "*":
+        print("1st No multiplied by 2nd is:  ", Op1 * Op2)
+    elif select == "/":
+        print("1st No divided by 2nd is: ", Op1 / Op2)
+    else:
+        print("Select from the given operators")
+        calculator(Op1, Op2)
+    return 0
+
+
+Operand1 = float(input("Enter 1st Number: "))
+Operand2 = float(input("Enter 2nd Number: "))
+calculator(Operand1, Operand2)
